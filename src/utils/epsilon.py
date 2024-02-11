06ac7f3f98e81d0fc1a5eeb5fa8a91a04ebd5168
Erik Řehulka <erik.rehulka@bratislava.sk>
2024-02-11 10:48:30 +0100

    performance update

--- src/utils/epsilon.py
+++ src/utils/epsilon.py
@@ -13,16 +13,19 @@ def calculateEpsilon(trees: list[Tree], otherEdges: list[Edge], dumbbells: list[
   treeRoots = list(map(lambda x: x.root, trees))
 
   for edge in otherEdges:
+    onEvenDepth1 = isInTreeOnEvenDepth(edge.v1, treeRoots)
+    onEvenDepth2 = isInTreeOnEvenDepth(edge.v2, treeRoots)
+
     # It must connect separate flowers
     if edge.v1.getTotalOuterFlower() == edge.v2.getTotalOuterFlower():
       continue
     # If one end is in a dumbbell and the other one at an even level in some tree, we can add only what the edge can take
-    if (isInDumbbell(edge.v1, dumbbells) and isInTreeOnEvenDepth(edge.v2, treeRoots)) or (isInDumbbell(edge.v2, dumbbells) and isInTreeOnEvenDepth(edge.v1, treeRoots)):
+    if (isInDumbbell(edge.v1, dumbbells) and onEvenDepth2) or (isInDumbbell(edge.v2, dumbbells) and onEvenDepth1):
       edgeEpsilon = edge.getEpsilon()
       if edgeEpsilon < epsilon:
         epsilon = edgeEpsilon
     # If both ends are at an even level in some tree (may be the same one), we can add only half what it can take
-    if isInTreeOnEvenDepth(edge.v1, treeRoots) and isInTreeOnEvenDepth(edge.v2, treeRoots):
+    if onEvenDepth1 and onEvenDepth2:
       edgeEpsilon = edge.getEpsilon() / 2
       if edgeEpsilon < epsilon:
         epsilon = edgeEpsilon
